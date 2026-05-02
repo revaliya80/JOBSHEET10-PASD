cfec1c2ac9c4b29a0cd2399217f489b9a60ba9a2
Refaliya-Vinandha <revaliya80@gmail.com>
2026-05-02 21:45:37 +0700

    Praktikum 2

---
+++ P2Jobsheet10/AntrianLayanan.java
@@ -0,0 +1,88 @@
+package P2Jobsheet10;
+
+public class AntrianLayanan {
+    Mahasiswa[] data;
+    int front;
+    int rear;
+    int size;
+    int max;
+
+    public AntrianLayanan(int n) {
+        max = n;
+        data = new Mahasiswa[max];
+        size = 0;
+        front = rear = -1;
+    }
+
+    public boolean isFull() {
+        if (size == max) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void tambahAntrian(Mahasiswa mhs) {
+        if (isFull()) {
+            System.out.println("Antrian penuh, tidak dapat menambah mahasiswa.");
+            return;
+        }
+
+        if (isEmpty()) {
+            front = rear = 0;
+        } else {
+            rear = (rear + 1) % max;
+        }
+
+        data[rear] = mhs;
+        size++;
+        System.out.println(mhs.nama + " berhasil masuk ke antrian.");
+    }
+
+    public Mahasiswa layaniMahasiswa() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+            return null;
+        }
+        Mahasiswa mhs = data[front];
+        front = (front + 1) % max;
+        size--;
+        return mhs;
+    }
+
+    public void lihatTerdepan() {
+        if (isEmpty()) {
+            System.out.println("Antrian Kosong.");
+        } else {
+            System.out.println("Mahasiswa terdepan: ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            data[front].tampilkanData();
+        }
+    }
+
+    public void tampilkanSemua() {
+        if (isEmpty()) {
+            System.out.println("Antrian Kosong.");
+            return;
+        }
+        System.out.println("Data Mahasiswa dalam Antrian:");
+        System.out.println("NIM - NAMA - PRODI - KELAS");
+        for (int i = 0; i < size; i++) {
+            int index = (front + i) % max;
+            System.out.print((i + 1) + ". ");
+            data[index].tampilkanData();
+        }
+    }
+
+    public int getJumlahAntrian() {
+        return size;
+    }
+}
